7c9f16f528da7574e1ff64b1482638f09a775bdc
Daniel da Silva Soares <daniel.soares@iatec.com>
2023-10-23 11:48:17 -0300

    second commit

--- Useless-lib/projects/useless-example/src/app/app.module.ts
+++ Useless-lib/projects/useless-example/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UselessLibModule } from '@poc/Useless-lib';
+import { UselessLibModule } from 'projects/useless-lib/src/public-api';
 
 @NgModule({
   declarations: [
